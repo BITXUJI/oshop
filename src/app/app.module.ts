51d3f0ce3a06098bd31398fea178fc8bdb29e221
bitxuji <bitxuji@gmail.com>
2023-12-09 22:10:03 +0900

    Creating a new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDRYbyclcUlvkm57lYTach_TtUhpZDi4eY",
+      authDomain: "oshop-9b2ca.firebaseapp.com",
+      projectId: "oshop-9b2ca",
+      storageBucket: "oshop-9b2ca.appspot.com",
+      messagingSenderId: "997564146672",
+      appId: "1:997564146672:web:8405c265f359fa8be8f335",
+      measurementId: "G-DQ7YR2D65J"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]

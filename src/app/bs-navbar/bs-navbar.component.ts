7ad12808c8c7ab9e604029acdc29cd68d98f3c74
bitxuji <bitxuji@gmail.com>
2023-12-11 20:42:44 +0900

    Implementing the LogOut

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'bs-navbar',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(x => console.log(x));
+  }
+  logout() {
+    this.afAuth.signOut();
+  }
 }

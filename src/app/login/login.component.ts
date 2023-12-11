f90050be6569b7ac67bc0e9046dd196cf22dd7ea
bitxuji <bitxuji@gmail.com>
2023-12-11 19:14:54 +0900

    Implementing Google Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { GoogleAuthProvider } from 'firebase/auth'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private afAuth: AngularFireAuth) { }
 
+  login() {
+    this.afAuth.signInWithPopup(new GoogleAuthProvider()).then(
+      (result) => {
+        console.log('You have been successfully logged in!');
+      }).catch((error) => {
+        console.log(error);
+      });
+    // this.afAuth.signInWithRedirect(new GoogleAuthProvider()).then(
+    //   (result) => {
+    //     console.log('You have been successfully logged in!');
+    //   }).catch((error) => {
+    //     console.log(error);
+    //   });
+  }
 }

e2a5d5cdabf7a36133cae064fb7516030a8eb524
bitxuji <bitxuji@gmail.com>
2023-12-17 16:47:57 +0900

    Rendering the list of product

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductService } from 'src/app/product.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent {
+  products$: Observable<any>;
 
+  constructor(private productService: ProductService) {
+    this.products$ = productService.getAll();
+  }
 }

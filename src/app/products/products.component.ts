2165ee89c8584a1431372a13acd0d3ca6db76ea4
bitxuji <bitxuji@gmail.com>
2023-12-21 23:51:51 +0900

    Displaying all the products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Observable, ObservedValueOf } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+  products$: Observable<any>;
 
+  constructor(productService: ProductService) {
+    this.products$ = productService.getAll();
+  }
 }

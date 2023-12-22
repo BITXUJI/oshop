6db3ad4a10c4eedb6accbfdea2a140fca31a1b22
bitxuji <bitxuji@gmail.com>
2023-12-22 18:43:11 +0900

    Filtering products by category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Observable } from 'rxjs';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -9,11 +10,28 @@ import { CategoryService } from '../category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$: Observable<any>;
-  categories$: Observable<any>
+  // products$: Observable<any>;
+  categories$: Observable<any>;
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  category: string = '';
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService,
+  ) {
+    productService.getAll().subscribe(products => this.products = products);
+
     this.categories$ = categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category') || '';
+
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.payload.val().category === this.category) :
+        this.products;
+    });
+
   }
 }
